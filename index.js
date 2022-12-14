7784e76b799883556f8e368595275e3b64814f30
devin <devinelliotgomez@gmail.com>
2022-12-13 19:33:22 -0600

    minor tweaks

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 let comingDays = "";
 let history =[];
 
-function citySearch() {
-    var searchedCity=document.getElementById("Input").value;
-    displaySearched(searchedCity);
-}
-
 function displaySearched(city) {
+
     var openWeatherApi = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=803edff7ea43ff42bf363994620dcd18&units=imperial`;
   
     fetch(openWeatherApi)
       .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
+      return response.json();
+      }) .then(function (data) {
        
-        var currentWeather = ` <div class="bg-dark"> <h4>Current Weather In: ${city}
-          ${moment.unix(data.list[0].dt).format("MM/DD/YY")}</h4>   
-            <img class="imgClass center"  src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png"></img>
-                <h4 class="container1 text-center">Temperature:${data.list[0].main.temp}</h4>
-                <h4 class="container1 text-center">Humidity:${data.list[0].main.humidity}</h4>
-                <h4 class="container1 text-center">Wind Speed:${data.list[0].wind.speed}</h4></div>`;
+        var currentWeather = 
+        ` <div class="bg-dark"> 
+            <h4>Current Weather In: ${city} ${moment.unix(data.list[0].dt).format("MM/DD/YY")}</h4>   
+            <img class="center" src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png"></img>
+            <h4 class="text-center">Temperature:${data.list[0].main.temp}</h4>
+            <h4 class="text-center">Humidity:${data.list[0].main.humidity}</h4>
+            <h4 class="text-center">Wind Speed:${data.list[0].wind.speed}</h4>
+          </div>`;
                 
         document.getElementById("currentDay").innerHTML = currentWeather;
 
         //current weather versus coming weather
 
         for (let i = 1; i <= 5; i++) {
-          comingDays += ` <div class=" weather col-2 m-3" >
+          comingDays += 
+        ` <div class="col-2 m-3" >
             ${moment.unix(data.list[i].dt).format("MM/DD/YY")}
-              <img class="imgClass" id="weatherIcon2" src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png"></img>
-                <h4 class="container2">Temperature:${data.list[i].main.temp}</h4>
-                <h4 class="container2">Humidity:${data.list[i].main.humidity}</h4>
-                <h4 class="container2">Wind Speed:${data.list[i].wind.speed}</h4>
-                                                                        </div>`;
-
-      document.getElementById("otherDays").innerHTML = comingDays;
-
+            <img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png"></img>
+            <h4>Temperature:${data.list[i].main.temp}</h4>
+            <h4>Humidity:${data.list[i].main.humidity}</h4>
+            <h4>Wind Speed:${data.list[i].wind.speed}</h4>
+          </div>`;
+            document.getElementById("otherDays").innerHTML = comingDays;
         }
       }
     )
   }
+
+function citySearch() {
+  var searchedCity=document.getElementById("Input").value;
+  displaySearched(searchedCity);
+}
 
 function searchHistory() {
   var inputval = document.getElementById("Input").value;
